7b3a9bd75c353b95cac4534a654cf600f81c41f2
yihsiu <s30401s@gmail.com>
2024-12-10 15:48:40 +0900

    chore: update Solidity version to 0.8.28 in mock contracts

--- contracts/mocks/MockErc1155.sol
+++ contracts/mocks/MockErc1155.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: UNLICENSED
-pragma solidity 0.8.27;
+pragma solidity 0.8.28;
 
 import {ERC1155} from "@openzeppelin/contracts/token/ERC1155/ERC1155.sol";
 
--- contracts/mocks/MockItem.sol
+++ contracts/mocks/MockItem.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: UNLICENSED
-pragma solidity 0.8.27;
+pragma solidity 0.8.28;
 
 import "@openzeppelin/contracts/token/ERC1155/ERC1155.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
